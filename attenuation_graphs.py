8bc7c8cdd363a59268fa5e0fdbffc73d8ac5b4dd
Archie Brooks <archiebrooks@Archies-MacBook-Pro-5.local>
2024-03-19 14:57:37 +0000

    added iron

--- attenuation_graphs.py
+++ attenuation_graphs.py
@@ -11,15 +11,25 @@ air = data[:,1]
 iron = data[:,2]
 lead = data[:,3]
 
-def fitting_fn(x,a,b):
-    return(a*(x**(b)))
+def fitting_fn(x,a,b,c):
+    return((a*(x**b))+c)
 
-coeff_fit, cov_fit = curve_fit(fitting_fn, energy, air, [8e-5,-0.492])
+air_coeff_fit, air_cov_fit = curve_fit(fitting_fn, energy, air, [8e-5,-0.492,0])
+iron_coeff_fit, iron_cov_fit = curve_fit(fitting_fn, energy, iron, [0.1,-2,-0.23])
 x = np.linspace(0.4,10,1000000)
-y = coeff_fit[0]*(x**coeff_fit[1])
+y_air = fitting_fn(x,air_coeff_fit[0], air_coeff_fit[1],air_coeff_fit[2])
+y_iron = fitting_fn(x,iron_coeff_fit[0],iron_coeff_fit[1],iron_coeff_fit[2])
 
 plt.figure()
 plt.plot(energy,air,"r.")
-plt.plot(x,y)
+plt.plot(x,y_air)
 plt.show()
 
+
+plt.figure()
+plt.plot(energy, iron, "r.")
+plt.plot(x,y_iron)
+plt.show()
+
+print(air_coeff_fit)
+print(iron_coeff_fit)
